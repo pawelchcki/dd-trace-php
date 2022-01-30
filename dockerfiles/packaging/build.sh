a0d23d1c143951e81ec243a4ac613d9e7b8f0931
Pawel Chojnacki <pawelchcki@gmail.com>
2022-01-30 15:18:00 +0100

    hmm weird

--- dockerfiles/packaging/build.sh
+++ dockerfiles/packaging/build.sh
@@ -5,7 +5,7 @@ CFLAGS=${CFLAGS:="-std=gnu11 -O2 -g -Wall -Wextra"}
 
 mkdir -p extensions
 switch-php $PHP_VERSION 
-make all ECHO_ARG="-e" CFLAGS="${CFLAGS}" BUILD_DIR="./"
+make all ECHO_ARG="-e" CFLAGS="${CFLAGS}" BUILD_DIR="."
 find -name "ddtrace.so"
 
 cp .libs/ddtrace.so extensions/ddtrace-${SO_SUFFIX}.so
